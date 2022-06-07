007877dc1185916f6aac5074763aab0bee707619
Almir <crafteasy@yandex.ru>
2022-06-08 01:18:40 +0300

    Eще задача с массивом

---
+++ Array2/Program.cs
@@ -0,0 +1,19 @@
+﻿// array.Length - возвращает длинну(колличество массива). "break" - прервать задачу
+
+int[] array = {1, 12, 31, 4, 15, 16, 17, 18 };
+
+int n = array.Length;
+int find = 31;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++;
+}
